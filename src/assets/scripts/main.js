26306ccfa837f5a2e73961bb2e6917b022a9fe64
Pepe Mateo <jdmatvaz@gmail.com>
2021-10-30 13:23:06 +0200

    Test react

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,25 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+import React from 'react'
+import { render } from 'react-dom'
+
+import '../styles/main.scss'
+
+const Home = () => (
+  <div className="uoc-container">
+    <picture>
+      <source srcSet="./assets/images/logo.png?as=webp&width=350" type="image/webp" />
+      <img src="./assets/images/logo.png?width=350" alt="test image" />
+    </picture>
+    <h1 className="uoc-title">Hello, UOC!</h1>
+    <p>
+      This is a starter template for the Advanced HTML and CSS Tools courses
+      from the Master's Program in Multimedia Applications and the Master's
+      Program in Web App and Website Development at the Universitat Oberta de
+      Catalunya.
+    </p>
+  </div>
+)
+
+render(<Home />, document.getElementById('app'))
