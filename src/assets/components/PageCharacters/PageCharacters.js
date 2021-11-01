8ad9d09fc6dc864c1bae15a826d7de123756c160
Pepe Mateo <jdmatvaz@gmail.com>
2021-11-01 01:42:40 +0100

    Test char object fix v2

--- src/assets/components/PageCharacters/PageCharacters.js
+++ src/assets/components/PageCharacters/PageCharacters.js
@@ -1,12 +1,12 @@
 import React from "react";
 import CardCharacter from "../CardCharacter"
 
-import char1 from '../images/01_char.jpeg'
-import char2 from '../images/02_char.jpeg'
-import char3 from '../images/03_char.jpeg'
-import char4 from '../images/04_char.jpeg'
-import char5 from '../images/05_char.jpeg'
-import char6 from '../images/06_char.jpeg'
+import char1 from '../../images/01_char.jpeg'
+import char2 from '../../images/02_char.jpeg'
+import char3 from '../../images/03_char.jpeg'
+import char4 from '../../images/04_char.jpeg'
+import char5 from '../../images/05_char.jpeg'
+import char6 from '../../images/06_char.jpeg'
 
 const characters = [
     {
