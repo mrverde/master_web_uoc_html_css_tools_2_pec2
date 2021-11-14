10746242e54c753dd0f6cb6e28e6f26b0624de9c
Pepe Mateo <jdmatvaz@gmail.com>
2021-11-14 21:25:21 +0100

    Added first version grid home

--- src/assets/components/PageHome/PageHome.js
+++ src/assets/components/PageHome/PageHome.js
@@ -1,8 +1,23 @@
 import React from "react";
+
+import poster from '../../images/actor-g1c051a08e_1920.jpg'
+import "./PageHome.scss"
+
+
 const PageHome = () => {
-    return <div>
-        <h1>The Orphan of Zhao</h1>
-    </div>
+    return <>
+        {/* <img className="home--background" src={poster} /> */}
+        <div className="home">
+            {/* <h1>The Orphan of Zhao</h1> */}
+            <div className="square square-1">1</div>
+            <div className="square square-2">
+                <div className="home--title">El huérfano <br /> de los <br /> Zhao </div>
+                <hr className="home--separator" />
+                <div className="home--subtitle"> Historia de un amarillo</div></div>
+            <div className="square square-3">3</div>
+            <div className="square square-4">4</div>
+        </div>
+    </>
 }
 
 export default PageHome;
