575991863237c92eb2d583210f7980ca459a466b
Pepe Mateo <jdmatvaz@gmail.com>
2021-11-20 13:25:25 +0100

    Added blog page

--- src/assets/pages/PageBlog/PageBlog.js
+++ src/assets/pages/PageBlog/PageBlog.js
@@ -1,74 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import chineseTemple from '../../images/buddha-tempel-1468928.jpg'
+import "./PageBlog.scss"
 
 const PageBlog = () => {
-    return <article>
-        <div className="jumbotron" >
-            <div className="container for-about">
-                <h1 className="display-4">Blog Article or Information / Bio Page</h1>
-                <p className="lead">by Author One</p>
+    return <article className="blog">
+        <div className="blog-jumbotron-container">
+            <div className="jumbotron blog-jumbotron" >
+                <div className="container for-about">
+                    <h1 className="display-4 blog-jumbotron-text">Blog Article or Information / Bio Page</h1>
+                    <p className="lead blog-jumbotron-author">by Author One</p>
+                </div>
             </div>
         </div>
+        <div className="blog-container">
+            <div className="blog-subcontainer">
+                <p className="blog-paragraph blog-paragraph_header">Far far away, behind the word mountains, far from the countries Vokalia and
+                    Consonantia, there live the blind texts.</p>
+                <blockquote className="blog-quote" cite="https://es.wikiquote.org/wiki/Buda_Gautama">
+                    <p className="blog-paragraph blog-paragraph_quote">
+                        Far far away, behind the word mountains, far from the countries Vokalia and
+                        Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
+                        at the coast of the Semantics, a large language ocean. Far far away, behind the
+                        word mountains.</p>
+                    <strong>Zu Ming Yu</strong>
+                </blockquote>
 
-        <h1>Blog Article or Information / Bio Page</h1>
-        <h5>by Author One</h5>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia and
-            Consonantia, there live the blind texts.</p>
-        <blockquote cite="https://es.wikiquote.org/wiki/Buda_Gautama">
-            <p>
-                Far far away, behind the word mountains, far from the countries Vokalia and
-                Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
-                at the coast of the Semantics, a large language ocean. Far far away, behind the
-                word mountains.
-                <strong>Zu Ming Yu</strong>
-            </p>
-        </blockquote>
+                <p className="blog-paragraph blog-paragraph_endline">Far far away, behind the word mountains, far from the countries Vokalia and
+                    Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
+                    at the coast of the Semantics, a large language ocean. Far far away, behind the
+                    word mountains. Far far away, behind the word mountains, far from the countries
+                    Vokalia and Consonantia, there live the blind texts.</p>
+                <hr className="mt-3 mb-3" />
+                <ol className="blog-list">
+                    <li className="blog-list-element">
+                        <h4 className="blog-list-element-header">Item One</h4>
+                        <p className="blog-paragraph">Far far away, behind the word mountains, far from the countries Vokalia and
+                            Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at
+                            the coast of the Semantics.</p>
+                    </li>
+                    <li className="blog-list-element">
+                        <h4 className="blog-list-element-header">Item Two</h4>
+                        <p className="blog-paragraph">Far far away, behind the word mountains, far from the countries Vokalia and
+                            Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at
+                            the coast of the Semantics, a large language ocean.</p>
+                    </li>
+                    <li className="blog-list-element">
+                        <h4 className="blog-list-element-header">Item Three</h4>
+                        <p className="blog-paragraph">Far far away, behind the word mountains, far from the countries Vokalia and
+                            Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at
+                            the coast of the Semantics, a large language ocean.</p>
+                    </li>
+                </ol>
+                <hr className="mt-3 mb-3" />
+                <p className="blog-paragraph blog-paragraph_header blog-paragraph_startline">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                <p className="blog-paragraph">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                    there live the blind texts. Separated they live in Bookmarksgrove right at the coast
+                    of the Semantics, a large language ocean. Far far away, behind the word mountains, far
+                    from the countries Vokalia and Consonantia, there live the blind texts. Separated they
+                    live in Bookmarksgrove</p>
+                <img className="blog-image" src={chineseTemple} alt="Chinese temple" />
+                <blockquote className="blog-quote" cite="https://es.wikiquote.org/wiki/Buda_Gautama">
+                    <p className="blog-paragraph blog-paragraph_quote">
+                        Far far away, behind the word mountains, far from the countries Vokalia and
+                        Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
+                        at the coast of the Semantics, a large language ocean. Far far away, behind the
+                        word mountains. </p><br />
+                    <strong>Zu Ming Yu</strong>
 
-        <p>Far far away, behind the word mountains, far from the countries Vokalia and
-            Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
-            at the coast of the Semantics, a large language ocean. Far far away, behind the
-            word mountains. Far far away, behind the word mountains, far from the countries
-            Vokalia and Consonantia, there live the blind texts.</p>
-        <hr className="mt-3 mb-3" />
-        <ol>
-            <li>
-                <h4>Item One</h4>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                    Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at
-                    the coast of the Semantics.</p>
-            </li>
-            <li>
-                <h4>Item Two</h4>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                    Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at
-                    the coast of the Semantics, a large language ocean.</p>
-            </li>
-            <li>
-                <h4>Item Three</h4>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                    Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at
-                    the coast of the Semantics, a large language ocean.</p>
-            </li>
-        </ol>
-        <hr className="mt-3 mb-3" />
-        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
-            there live the blind texts. Separated they live in Bookmarksgrove right at the coast
-            of the Semantics, a large language ocean. Far far away, behind the word mountains, far
-            from the countries Vokalia and Consonantia, there live the blind texts. Separated they
-            live in Bookmarksgrove</p>
-        <img src={chineseTemple} alt="Chinese temple" />
-        <blockquote cite="https://es.wikiquote.org/wiki/Buda_Gautama">
-            <p>
-                Far far away, behind the word mountains, far from the countries Vokalia and
-                Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right
-                at the coast of the Semantics, a large language ocean. Far far away, behind the
-                word mountains.
-                <strong>Zu Ming Yu</strong>
-            </p>
-        </blockquote>
-
+                </blockquote>
+            </div>
+        </div>
     </article>
 }
 
--- src/assets/pages/PageHome/PageHome.js
+++ src/assets/pages/PageHome/PageHome.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import poster from '../../images/actor-g1c051a08e_1920.jpg'
 import "./PageHome.scss"
 
 
 const PageHome = () => {
+    useEffect(() => {
+        document.body.classList.add("overflowy-hidden");
+        return () => {
+            document.body.classList.remove("overflowy-hidden");
+        };
+    }, []);
+
     return <>
         {/* <img className="home-background" src={poster} /> */}
         <div className="home">
             {/* <h1>The Orphan of Zhao</h1> */}
             <div className="square square-1">1</div>
             <div className="square square-2">
-                <div className="home-title">El huérfano <br /> de los <br /> Zhao </div>
+                <div className="home-title">The <br />  Orphan <br />of Zhao </div>
                 <hr className="home-separator" />
-                <div className="home-subtitle"> Historia de un amarillo</div></div>
+                <div className="home-subtitle">A yellow history</div></div>
             <div className="square square-3">3</div>
             <div className="square square-4">4</div>
         </div>
